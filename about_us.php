4c463269c92c99591e1a735c65aa711f558daf83
Avanish Sah <sahawnish2014@gmail.com>
2024-12-31 14:10:55 +0545

    initial project production for smarika's canteen protal

---
+++ about_us.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About US</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <style type="text/css">
+       body{
+       background-image: url("images/bmc2.jpg");
+       background-size: 100%;
+       background-repeat: no-repeat;
+
+    }
+    
+  </style>
+</head>
+<body>
+  <h1 align="center" style="color: #ffffff">THE TEAM</h1>
+
+  
+
+
+  <div class="w3-row-padding w3-padding-16 w3-center" style="color:white">
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/Rajiv.jpg" alt="Kartik" style="width: 350px;height: 350px">
+      <h3>Smarika Bhatta</h3>
+      <p> Developer</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/Rajiv.jpg" alt="Kalpalatika" style="width: 350px;height: 350px">
+      <h3>Smarika's Friend</h3>
+      <p>System Architect</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/Rajiv.jpg" alt="Kumaran" style="width: 350px;height: 350px">
+      <h3>Smarika's Friend</h3>
+      <p>Front End Developer</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/Rajiv.jpg" alt="Ajit" style="width: 350px;height: 350px">
+      <h3>Smarika's Friend</h3>
+      <p>Front End Developer</p>
+    </div>
+
+
+<p style="color:white">&copy; 2017 SIES .GST&middot; CMS</p>
+  
+
+</body>
+</html>
+
+
